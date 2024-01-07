351c7a099385a56d28e2d06abe2e462522bd7a8b
averybrubeck <averybrubeck@yahoo.com>
2024-01-07 12:02:01 -0500

    using ruby2d created window with bg image and sprite

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,6 +1,73 @@
-# open the file
-# select a word between 5 & 10 chars
-# save word as secret
+require 'ruby2d'
+
+class Hangman
+  def initialize
+    Window.set(title: 'Hangman', width: 840, height: 480)
+    Image.new('background.png')
+    create_man
+    Window.show
+  end
+
+  def create_man
+    @head = Circle.new(
+      x: 200, y: 100,
+      radius: 20,
+      sectors: 32,
+      color: 'red'
+    )
+
+    @body = Line.new(
+      x1: 200, y1: 120,
+      x2: 200, y2: 200,
+      width: 5,
+      color: 'red'
+    )
+
+    @left_leg = Line.new(
+      x1: 200, y1: 200,
+      x2: 180, y2: 240,
+      width: 5,
+      color: 'red'
+    )
+
+    @right_leg = Line.new(
+      x1: 200, y1: 200,
+      x2: 220, y2: 240,
+      width: 5,
+      color: 'red'
+    )
+
+    @left_arm = Line.new(
+      x1: 200, y1: 140,
+      x2: 180, y2: 180,
+      width: 5,
+      color: 'red'
+    )
+
+    @right_arm = Line.new(
+      x1: 200, y1: 140,
+      x2: 220, y2: 180,
+      width: 5,
+      color: 'red'
+    )
+  end
+end
+game = Hangman.new
+game
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def choose_secret
   secret = nil
@@ -14,4 +81,3 @@ def choose_secret
   end
   puts secret
 end
-choose_secret
