938b4eb3e2edc60215d146b48386695e41a94002
averybrubeck <averybrubeck@yahoo.com>
2024-01-08 21:11:33 -0500

    added update  board and fixed styling

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -2,7 +2,7 @@
 class Hangman
   def initialize
     @secret = choose_secret.downcase
-    @lives = 9
+    @turns = 9
     @guess = []
     puts "Welcome to Hangman! You have #{@lives} lives to guess the secret word."
     generate_board
@@ -33,14 +33,43 @@ class Hangman
     end
     if @secret.include? @guess
       puts 'Your Guess Is Correct'
+      update_board
     else
       puts 'Your Guess Is Incorrect Try again'
       @turns -= 1
+      hang_the_man
     end
   end
 
   def update_board
+    updated_board = ''
+    @secret.each_char do |c|
+      updated_board += @guess.include?(c) ? c + ' ' : '_ '
+    end
+    puts updated_board.strip
+  end
 
+  def hang_the_man
+    case @turns
+    when 8
+      print 'The head has been hung'
+    when 7
+      print 'The body has been hung'
+    when 6
+      print 'The left leg has been hung'
+    when 5
+      print 'The right leg has been hung'
+    when 4
+      print 'The leg arm has been hung'
+    when 3
+      print 'The right arm has been hung'
+    when 2
+      print 'The face has been drawn'
+    when 1
+      print 'The man is almost dead, better hurry!'
+    when @turns.zero?
+      print 'The man is dead'
+    end
   end
 end
 
